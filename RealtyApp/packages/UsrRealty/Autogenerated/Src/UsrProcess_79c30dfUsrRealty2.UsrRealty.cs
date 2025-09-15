99eb43eca7b2688d2c0999877766deb941c7ec9d
Mohamed Sabek <mmsabek99@gmail.com>
2025-09-15 13:20:47 +0300

    add usrRealty app

---
+++ RealtyApp/packages/UsrRealty/Autogenerated/Src/UsrProcess_79c30dfUsrRealty2.UsrRealty.cs
@@ -0,0 +1,68 @@
+﻿namespace Terrasoft.Core.Process
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Drawing;
+	using System.Globalization;
+	using System.Text;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+	using Terrasoft.Core.DB;
+	using Terrasoft.Core.Entities;
+	using Terrasoft.Core.Process;
+	using Terrasoft.Core.Process.Configuration;
+
+	#region Class: UsrProcess_79c30dfUsrRealty2MethodsWrapper
+
+	/// <exclude/>
+	public class UsrProcess_79c30dfUsrRealty2MethodsWrapper : ProcessModel
+	{
+
+		public UsrProcess_79c30dfUsrRealty2MethodsWrapper(Process process)
+			: base(process) {
+			AddScriptTaskMethod("ScriptTask1Execute", ScriptTask1Execute);
+		}
+
+		#region Methods: Private
+
+		private bool ScriptTask1Execute(ProcessExecutingContext context) {
+			// Step 1: Get input parameter
+			Guid realtyId = Get<Guid>("RealtyIdParameter");
+			
+			// Step 2: Get the schema manager and the RealtyVisit entity
+			var schema = UserConnection.EntitySchemaManager.GetInstanceByName("UsrRealtyVisit");
+			
+			// Step 3: Set starting visit date (next day)
+			DateTime visitDate = DateTime.Now.AddDays(1);
+			
+			if (realtyId == Guid.Empty) {
+			    throw new Exception("RealtyIdParameter is missing or invalid.");
+			}
+			
+			
+			for (int i = 0; i < 3; i++) {
+			    var visitEntity = schema.CreateEntity(UserConnection);
+			    visitEntity.SetDefColumnValues();
+			    visitEntity.SetColumnValue("UsrRealtyParentId", realtyId);       
+			    visitEntity.SetColumnValue("UsrVisitDateTime", visitDate);       
+			    visitEntity.Save();
+			
+			    // Prepare date for next record
+			    visitDate = visitDate.AddDays(1);
+			}
+			
+			
+			return true;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
---
+++ RealtyApp/packages/UsrRealty/Autogenerated/Src/UsrRealtyServiceSchema.UsrRealty.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrRealtyServiceSchema
+
+	/// <exclude/>
+	public class UsrRealtyServiceSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrRealtyServiceSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrRealtyServiceSchema(UsrRealtyServiceSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("0c2e6e49-a809-45c1-b55c-ee0fd1096b5f");
+			Name = "UsrRealtyService";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("227b5ba4-da71-4d6e-8a86-21093a04c074");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,211,215,87,40,201,200,44,86,0,162,68,133,228,252,220,220,212,188,18,0,198,140,41,59,20,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("0c2e6e49-a809-45c1-b55c-ee0fd1096b5f"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
